83bcec3ee4c5ac650242de9f42af52ff946171ba
Leandro Andreaci <leandroandreaci@hotmail.com>
2016-08-18 15:24:34 -0300

    change folder name

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 
 require 'vendor/autoload.php';
 
+/* ADAPTOR */
+use App\Adapter\Book;
 use App\Adapter\Kindle;
 use App\Adapter\eReaderAdapter;
+use App\Adapter\Nook;
 use App\Adapter\Person;
 
 (new Person)->read(new eReaderAdapter(new Kindle()));
+(new Person)->read(new eReaderAdapter(new Nook()));
+(new Person)->read(new eReaderAdapter(new Book()));
 
